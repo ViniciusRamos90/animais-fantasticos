a0ff0685895fe731b655f20ccead2f1ff1f93e10
Vinicius Ramos <vluizramos@gmail.com>
2024-02-19 15:37:13 -0300

    Primeiro commit

---
+++ js/modules/accordion.js
@@ -0,0 +1,17 @@
+export default function initAccordion() {
+  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
+  const classAtivo = 'active';
+
+  function activeAccordion() {
+    this.classList.toggle(classAtivo);
+    this.nextElementSibling.classList.toggle(classAtivo);
+  }
+  if (accordionList.length) {
+    accordionList[0].classList.add(classAtivo);
+    accordionList[0].nextElementSibling.classList.add(classAtivo);
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }
+}
